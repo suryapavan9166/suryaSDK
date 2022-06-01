c707e2d3027e5f7496d85245a494a102d97f186b
suryapavan9166 <suryapavan@peoplelinkvc.com>
2022-06-01 12:07:33 +0530

    surya SDK

---
+++ app/suryasdk/src/main/java/com/peoplelink/suryasdk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.peoplelink.suryasdk
+
+class MainActivity {
+
+    fun addNumbers(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun mulNumbers(a: Int, b: Int): Int {
+        return a * b
+    }
+}
